971ba08d356ffc4939c1e2922004437bdf496755
Alex C. Isbrecht <ISBRECHA@mail.gvsu.edu>
2024-03-14 12:58:52 -0400

    init

---
+++ MLProject/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.nudNightsStaying = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.picMap = New System.Windows.Forms.PictureBox()
+        CType(Me.nudNightsStaying, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picMap, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(278, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(276, 33)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Airbnb Price Estimator"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(342, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(138, 27)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "NYC Edition"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(296, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(234, 19)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "How many nights will you be staying?"
+        '
+        'nudNightsStaying
+        '
+        Me.nudNightsStaying.Location = New System.Drawing.Point(300, 113)
+        Me.nudNightsStaying.Name = "nudNightsStaying"
+        Me.nudNightsStaying.Size = New System.Drawing.Size(215, 20)
+        Me.nudNightsStaying.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(296, 152)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(219, 19)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Do you want the place to yourself?"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Yes, I want the whole house to myself", "Yes, but I'm ok with a hotel room", "Yes, but I'm ok with just a room", "No, I'm ok sharing the room with others"})
+        Me.ComboBox1.Location = New System.Drawing.Point(300, 184)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(215, 21)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'picMap
+        '
+        Me.picMap.Image = CType(resources.GetObject("picMap.Image"), System.Drawing.Image)
+        Me.picMap.InitialImage = CType(resources.GetObject("picMap.InitialImage"), System.Drawing.Image)
+        Me.picMap.Location = New System.Drawing.Point(115, 3)
+        Me.picMap.Name = "picMap"
+        Me.picMap.Size = New System.Drawing.Size(788, 622)
+        Me.picMap.TabIndex = 7
+        Me.picMap.TabStop = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDark
+        Me.ClientSize = New System.Drawing.Size(1008, 637)
+        Me.Controls.Add(Me.picMap)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.nudNightsStaying)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        CType(Me.nudNightsStaying, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picMap, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents nudNightsStaying As NumericUpDown
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents picMap As PictureBox
+End Class
---
+++ MLProject/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class frmMain
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Me.MaximumSize = New Size(1024, 1000)
+
+    End Sub
+End Class
